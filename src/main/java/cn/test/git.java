e9b1493e95cf684b8ee2a188188d9203bd663d79
zxm872 <49151595+zxm872@users.noreply.github.com>
2019-11-12 19:57:43 +0800

    Update git.java

--- src/main/java/cn/test/git.java
+++ src/main/java/cn/test/git.java
@@ -3,6 +3,6 @@ package cn.test;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello");
-       System.out.println("你好啊")
+       System.out.println("你好啊");
     }
 }
