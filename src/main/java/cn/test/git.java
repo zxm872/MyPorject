03884ed88b21d110a0b940756b3194c2e966098c
zxm872 <910995823@qq.com>
2019-11-12 20:23:00 +0800

    2.增加输出语句

--- src/main/java/cn/test/git.java
+++ src/main/java/cn/test/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello");
        System.out.println("你好啊");
+        System.out.println("6666");
     }
 }
